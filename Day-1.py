e63a74492b83531387c640fcea68b67736886114
khuara17 <vishalkamlapure.be@gmail.com>
2022-03-11 18:09:33 +0530

    Day 1 - 9 March - Varibles and data types

---
+++ Day-1.py
@@ -0,0 +1,24 @@
+###### Variables & Basic Datatypes ##########
+
+a = 70 # Int
+b = 4.5 # Float
+c = 'python'
+d = "Python"
+e = True
+g = False
+print(  type(e)   )
+
+######## Comments ##################
+
+# Syntax :- # , ''' comments ''' , """ comments """
+
+# This is python leture
+'''
+This is python leture
+python is good lang
+i love python
+'''
+
+"""This is python leture
+python is good lang
+i love python"""
